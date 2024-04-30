996418dbd2ef17e819097719b8a1215c591f25b8
yy <1667253917@qq.com>
2024-04-30 22:39:36 +0800

    init

---
+++ cmake-build-debug-remote-host/src/CMakeFiles/rmdb.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/yy/Project/db2023/rmdb/src/rmdb.cpp" "src/CMakeFiles/rmdb.dir/rmdb.cpp.o" "gcc" "src/CMakeFiles/rmdb.dir/rmdb.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/yy/Project/db2023/rmdb/cmake-build-debug-remote-host/src/parser/CMakeFiles/parser.dir/DependInfo.cmake"
+  "/home/yy/Project/db2023/rmdb/cmake-build-debug-remote-host/src/execution/CMakeFiles/execution.dir/DependInfo.cmake"
+  "/home/yy/Project/db2023/rmdb/cmake-build-debug-remote-host/src/optimizer/CMakeFiles/planner.dir/DependInfo.cmake"
+  "/home/yy/Project/db2023/rmdb/cmake-build-debug-remote-host/src/analyze/CMakeFiles/analyze.dir/DependInfo.cmake"
+  "/home/yy/Project/db2023/rmdb/cmake-build-debug-remote-host/src/system/CMakeFiles/system.dir/DependInfo.cmake"
+  "/home/yy/Project/db2023/rmdb/cmake-build-debug-remote-host/src/record/CMakeFiles/record.dir/DependInfo.cmake"
+  "/home/yy/Project/db2023/rmdb/cmake-build-debug-remote-host/src/transaction/CMakeFiles/transaction.dir/DependInfo.cmake"
+  "/home/yy/Project/db2023/rmdb/cmake-build-debug-remote-host/src/recovery/CMakeFiles/recovery.dir/DependInfo.cmake"
+  "/home/yy/Project/db2023/rmdb/cmake-build-debug-remote-host/src/index/CMakeFiles/index.dir/DependInfo.cmake"
+  "/home/yy/Project/db2023/rmdb/cmake-build-debug-remote-host/src/storage/CMakeFiles/storage.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
